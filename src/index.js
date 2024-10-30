ac637c55b4e75dd54832c4481bf1c896f30e8742
Kafando <kafandoaime017@gmail.com>
2024-10-30 13:29:42 +0100

    Premier commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Index from './routes/Index';
+import { ToastContainer } from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'react-toastify/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Index />
+    <ToastContainer/>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
